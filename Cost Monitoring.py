909b3ca47f095f5dcfa0696c80473c442b0a19c8
Darius Tay <darius.tay@mile.cloud>
2024-06-24 17:42:52 +0800

    Add files via upload

---
+++ Cost Monitoring.py
@@ -0,0 +1,362 @@
+##### 0. Setup #####
+
+# Import packages
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from datetime import datetime, timedelta
+from snowflake.snowpark.context import get_active_session
+
+# Get Snowflake session
+session = get_active_session()
+st.title('Cost Monitoring Dashboard 📊')
+st.write("\n\n")
+
+##### 1. Filters #####
+
+# Accounts query
+acc_query = '''
+    SELECT DISTINCT(ACCOUNT_NAME) 
+    FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY
+'''
+
+# Convert to DataFrame and extract accounts
+df_acc = session.sql(acc_query).to_pandas()
+acc_list = sorted(df_acc['ACCOUNT_NAME'].tolist())
+
+# Define default date range (i.e., previous 1 week)
+default_end_date = datetime.today().date()
+default_start_date = default_end_date - timedelta(days = 7)
+
+# Service types query
+service_query = '''
+    SELECT DISTINCT(SERVICE_TYPE) 
+    FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY
+'''
+
+# Convert to DataFrame and extract service types
+df_service = session.sql(service_query).to_pandas()
+service_list = sorted(df_service['SERVICE_TYPE'].tolist())
+
+# Create filters
+filter1, filter2, filter3 = st.columns(3)
+
+## Accounts
+with filter1:
+    selected_acc = st.multiselect(
+        label = 'Select Accounts:', 
+        options = acc_list,
+        default = acc_list
+    )
+
+## Date range
+with filter2:
+    selected_date = st.date_input(
+        label= 'Select Date Range:', 
+        value = [default_start_date, default_end_date]
+    )
+
+## Service types
+with filter3:
+    selected_service = st.multiselect(
+        label = 'Select Service Types:', 
+        options = service_list,
+        default = service_list
+    )
+
+
+##### 2. Overall Cost #####
+
+st.divider()
+st.subheader('Overall Cost')
+
+# Quote each account name
+quoted_selected_acc = ["'{}'".format(acc) for acc in selected_acc]
+
+# Quote each service type
+quoted_selected_service = ["'{}'".format(service) for service in selected_service]
+
+# Extract start and end dates from selected_date
+start_date, end_date = selected_date
+start_date_str = start_date.strftime('%Y-%m-%d')
+end_date_str = end_date.strftime('%Y-%m-%d')
+
+# Total cost query
+if quoted_selected_acc and quoted_selected_service:
+    cost_query = '''
+        SELECT 
+            SUM(USAGE_IN_CURRENCY) AS TOTAL_COST_USD,
+            SUM(USAGE) AS TOTAL_COST_CREDITS,
+            AVG(USAGE_IN_CURRENCY) AS AVG_COST_USD,
+            AVG(USAGE) AS AVG_COST_CREDITS,
+        FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY 
+        WHERE ACCOUNT_NAME IN ({})
+        AND SERVICE_TYPE IN ({})
+        AND USAGE_DATE BETWEEN '{}' AND '{}'
+    '''.format(
+        ', '.join(quoted_selected_acc),
+        ', '.join(quoted_selected_service),
+        start_date_str,
+        end_date_str
+    )
+else:
+    cost_query = '''
+        SELECT 
+            0 AS TOTAL_COST_USD,
+            0 AS TOTAL_COST_CREDITS,
+            0 AS AVG_COST_USD,
+            0 AS AVG_COST_CREDITS
+    '''
+
+# Convert to DataFrame
+df_cost = session.sql(cost_query).to_pandas()
+
+# Extract total cost (USD)
+total_cost_usd = df_cost['TOTAL_COST_USD'].iloc[0]
+formatted_total_cost_usd = "${:,.2f}".format(total_cost_usd)
+
+# Extract total cost (credits)
+total_cost_credits = df_cost['TOTAL_COST_CREDITS'].iloc[0]
+formatted_total_cost_credits = "{:,.2f}".format(total_cost_credits)
+
+# Extract total cost (USD)
+avg_cost_usd = df_cost['AVG_COST_USD'].iloc[0]
+formatted_avg_cost_usd = "${:,.2f}".format(avg_cost_usd)
+
+# Extract total cost (credits)
+avg_cost_credits = df_cost['AVG_COST_CREDITS'].iloc[0]
+formatted_avg_cost_credits = "{:,.2f}".format(avg_cost_credits)
+
+# Create overall cost visual
+cost1, cost2, cost3, cost4 = st.columns(4)
+
+## Total cost in USD
+with cost1:
+    st.metric(
+        label = 'Total Cost in USD:',
+        value = formatted_total_cost_usd
+    )
+    
+## Total cost in credits
+with cost2:
+    st.metric(
+        label = 'Total Cost in Credits:',
+        value = formatted_total_cost_credits
+    )
+
+## Average cost in USD
+with cost3:
+    st.metric(
+        label = 'Average Cost in USD:',
+        value = formatted_avg_cost_usd
+    )
+    
+## Average cost in credits
+with cost4:
+    st.metric(
+        label = 'Average Cost in Credits:',
+        value = formatted_avg_cost_credits
+    )
+
+
+##### 3. Graphs #####
+
+### 3.1 Daily Cost ###
+
+st.divider()
+st.subheader('Daily Cost')
+
+# Daily cost query
+if quoted_selected_acc and quoted_selected_service:
+    daily_cost_query = '''
+        SELECT 
+            USAGE_DATE,
+            SUM(USAGE_IN_CURRENCY) AS TOTAL_COST_USD
+        FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY 
+        WHERE ACCOUNT_NAME IN ({})
+        AND SERVICE_TYPE IN ({})
+        AND USAGE_DATE BETWEEN '{}' AND '{}'
+        GROUP BY USAGE_DATE
+        ORDER BY USAGE_DATE ASC
+    '''.format(
+        ', '.join(quoted_selected_acc),
+        ', '.join(quoted_selected_service),
+        start_date_str,
+        end_date_str
+    )
+else:
+    daily_cost_query = '''
+        SELECT 
+            USAGE_DATE,
+            0 AS TOTAL_COST_USD
+        FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY 
+        WHERE USAGE_DATE BETWEEN '{}' AND '{}'
+        GROUP BY USAGE_DATE
+        ORDER BY USAGE_DATE ASC
+    '''.format(
+        start_date_str,
+        end_date_str
+    )
+
+# Convert to DataFrame
+df_daily_cost = session.sql(daily_cost_query).to_pandas()
+
+# Create a Plotly bar chart
+fig_daily_cost = px.bar(df_daily_cost, x = 'USAGE_DATE', y = 'TOTAL_COST_USD', 
+             labels = {'USAGE_DATE': 'Usage Date', 'TOTAL_COST_USD': 'Total Cost (USD)'},
+             title = 'Daily Cost in USD')
+
+fig_daily_cost.update_layout(
+    xaxis_title = '',
+    yaxis_title = '',
+    xaxis_tickformat = '%Y-%m-%d',
+    bargap = 0.2,
+    width = 800,
+    height = 400,
+    title = {
+        'text': 'Daily Cost in USD',
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    }
+)
+
+# Display the Plotly chart in Streamlit
+st.plotly_chart(fig_daily_cost, use_container_width = True)
+
+
+### 3.2 Cost by Account / Service Type ###
+
+st.subheader('Cost Breakdown')
+
+# Cost by account query
+if quoted_selected_acc and quoted_selected_service:
+    cost_acc_query = '''
+        SELECT 
+            ACCOUNT_NAME,
+            SUM(USAGE_IN_CURRENCY) AS TOTAL_COST_USD
+        FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY 
+        WHERE ACCOUNT_NAME IN ({})
+        AND SERVICE_TYPE IN ({})
+        AND USAGE_DATE BETWEEN '{}' AND '{}'
+        GROUP BY ACCOUNT_NAME
+        ORDER BY TOTAL_COST_USD DESC
+    '''.format(
+        ', '.join(quoted_selected_acc),
+        ', '.join(quoted_selected_service),
+        start_date_str,
+        end_date_str
+    )
+else:
+    cost_acc_query = '''
+        SELECT 
+            NULL AS ACCOUNT_NAME,
+            0 AS TOTAL_COST_USD
+    '''
+
+# Cost by service type query
+if quoted_selected_acc and quoted_selected_service:
+    cost_service_query = '''
+        SELECT 
+            SERVICE_TYPE,
+            SUM(USAGE_IN_CURRENCY) AS TOTAL_COST_USD
+        FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY 
+        WHERE ACCOUNT_NAME IN ({})
+        AND SERVICE_TYPE IN ({})
+        AND USAGE_DATE BETWEEN '{}' AND '{}'
+        GROUP BY SERVICE_TYPE
+        ORDER BY TOTAL_COST_USD DESC
+    '''.format(
+        ', '.join(quoted_selected_acc),
+        ', '.join(quoted_selected_service),
+        start_date_str,
+        end_date_str
+    )
+else:
+    cost_service_query = '''
+        SELECT 
+            NULL AS SERVICE_TYPE,
+            0 AS TOTAL_COST_USD
+    '''
+
+# Convert to DataFrame
+df_cost_acc = session.sql(cost_acc_query).to_pandas()
+df_cost_service = session.sql(cost_service_query).to_pandas()
+
+# Create the 1st Plotly pie chart: Cost by account
+fig_acc = px.pie(df_cost_acc, values = 'TOTAL_COST_USD', names = 'ACCOUNT_NAME', 
+             title = 'Cost by Account')
+
+fig_acc.update_layout(
+    title = {
+        'text': 'Cost by Account',
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    }
+)
+
+# Create the 2nd Plotly pie chart: Cost by service type
+fig_service = px.pie(df_cost_service, values = 'TOTAL_COST_USD', names = 'SERVICE_TYPE', 
+                     title = 'Cost by Service Type')
+
+fig_service.update_layout(
+    title = {
+        'text': 'Cost by Service Type',
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    }
+)
+
+# Display the Plotly charts side by side in Streamlit
+pie1, pie2 = st.columns(2)
+
+with pie1:
+    st.plotly_chart(fig_acc, use_container_width = True)
+
+with pie2:
+    st.plotly_chart(fig_service, use_container_width = True)
+
+
+### 3.3 Top 10 cost by Account AND Service Type ###
+
+st.subheader('Top Cost by Account & Service Type')
+st.write("\n\n")
+
+# Cost by account AND service typequery
+if quoted_selected_acc and quoted_selected_service:
+    cost_acc_service_query = '''
+        SELECT 
+            ACCOUNT_NAME,
+            SERVICE_TYPE,
+            SUM(USAGE_IN_CURRENCY) AS TOTAL_COST_USD
+        FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY 
+        WHERE ACCOUNT_NAME IN ({})
+        AND SERVICE_TYPE IN ({})
+        AND USAGE_DATE BETWEEN '{}' AND '{}'
+        GROUP BY ACCOUNT_NAME, SERVICE_TYPE
+        ORDER BY TOTAL_COST_USD DESC
+        LIMIT 10
+    '''.format(
+        ', '.join(quoted_selected_acc),
+        ', '.join(quoted_selected_service),
+        start_date_str,
+        end_date_str
+    )
+else:
+    cost_acc_service_query = '''
+        SELECT 
+            NULL AS ACCOUNT_NAME,
+            NULL AS SERVICE_TYPE,
+            0 AS TOTAL_COST_USD
+    '''
+
+# Convert to DataFrame
+df_cost_acc_service = session.sql(cost_acc_service_query).to_pandas()
+
+# Display the filtered DataFrame
+st.dataframe(df_cost_acc_service, use_container_width = True)
---
+++ Data Quality Check.py
@@ -0,0 +1,231 @@
+##### 0. Setup #####
+
+# Import packages
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from snowflake.snowpark.context import get_active_session
+
+# Get Snowflake session
+session = get_active_session()
+st.title('Data Quality Check 🔢')
+st.write("\n\n")
+
+##### 1. Filters #####
+
+# Create filters
+filter1, filter2, filter3, filter4 = st.columns(4)
+
+## Database
+with filter1:
+    selected_db = st.text_input(
+        label = 'Insert Database:',
+        value = 'FROSTBYTE_TASTY_BYTES'
+    )
+
+## Schema
+with filter2:
+    selected_schema = st.text_input(
+        label= 'Insert Schema:',
+        value = 'RAW_POS'
+    )
+
+## Table
+with filter3:
+    selected_tb = st.text_input(
+        label = 'Insert Table:',
+        value = 'TRUCK'
+    )
+
+## Column
+with filter4:
+    selected_col = st.text_input(
+        label = 'Insert Column:',
+        value = 'TRUCK_ID'
+    )
+
+
+##### 2. Create Views #####
+
+# Query: Unique and duplicate count view
+if selected_db and selected_schema and selected_tb and selected_col:
+    uni_dup_cnt_view_query = '''
+        CREATE OR REPLACE VIEW {0}.{1}.{2}_{3}_UNIQUE_DUPLICATE_COUNT AS
+        SELECT
+          COUNT(DISTINCT {3}) AS UNIQUE_COUNT,
+          COUNT({3}) - COUNT(DISTINCT {3}) AS DUPLICATE_COUNT
+        FROM {0}.{1}.{2};
+    '''.format(
+        selected_db,
+        selected_schema,
+        selected_tb,
+        selected_col
+    )
+    
+    # Execute the query
+    session.sql(uni_dup_cnt_view_query).collect()
+
+
+##### 3. Data Qaulity Summary #####
+
+st.divider()
+st.subheader('Column Overview')
+
+# Query the unique and duplicate count view
+if selected_db and selected_schema and selected_tb and selected_col:
+    uni_dup_cnt_select_query = '''
+        SELECT * FROM {0}.{1}.{2}_{3}_UNIQUE_DUPLICATE_COUNT;
+    '''.format(
+        selected_db,
+        selected_schema,
+        selected_tb,
+        selected_col
+    )
+else:
+    uni_dup_cnt_select_query = '''
+        SELECT
+          0 AS UNIQUE_COUNT,
+          0 AS DUPLICATE_COUNT
+    '''
+
+# Convert to DataFrame
+df_uni_dup_cnt = session.sql(uni_dup_cnt_select_query).to_pandas()
+
+# Create data quality summary visual
+uni_cnt, dup_cnt = st.columns(2)
+
+## Total cost in USD
+with uni_cnt:
+    st.metric(
+        label = 'No. of Unqiue Rows:',
+        value = df_uni_dup_cnt['UNIQUE_COUNT']
+    )
+    
+## Total cost in credits
+with dup_cnt:
+    st.metric(
+        label = 'No. of Duplicate Rows:',
+        value = df_uni_dup_cnt['DUPLICATE_COUNT']
+    )
+
+
+##### 4. Load History #####
+
+st.divider()
+st.subheader('Table Load History')
+
+# Successful load history query
+if selected_db and selected_schema and selected_tb:
+    load_hist_success_query ='''
+        SELECT 
+            CATALOG_ID,
+            CATALOG_NAME,
+            SCHEMA_ID,
+            SCHEMA_NAME,
+            TABLE_ID,
+            TABLE_NAME,
+            FILE_NAME,
+            LAST_LOAD_TIME,
+            STATUS,
+            ROW_COUNT,
+            ROW_PARSED
+        FROM SNOWFLAKE.ACCOUNT_USAGE.LOAD_HISTORY
+        WHERE CATALOG_NAME = '{}' 
+        AND SCHEMA_NAME = '{}'
+        AND TABLE_NAME = '{}'
+        AND STATUS = 'LOADED'
+        ORDER BY LAST_LOAD_TIME ASC
+    '''.format(
+        selected_db,
+        selected_schema,
+        selected_tb
+    )
+else:
+    load_hist_success_query ='''
+        SELECT 
+            NULL AS CATALOG_ID,
+            NULL AS CATALOG_NAME,
+            NULL AS SCHEMA_ID,
+            NULL AS SCHEMA_NAME,
+            NULL AS TABLE_ID,
+            NULL AS TABLE_NAME,
+            NULL AS FILE_NAME,
+            NULL AS LAST_LOAD_TIME,
+            NULL AS STATUS,
+            0 AS ROW_COUNT,
+            0 AS ROW_PARSED
+    '''
+
+# Convert to DataFrame
+df_load_hist_success = session.sql(load_hist_success_query).to_pandas()
+
+# Create a Plotly bar chart
+fig_load_hist_success = px.bar(df_load_hist_success, x = 'LAST_LOAD_TIME', y = 'ROW_PARSED', 
+             labels = {'LAST_LOAD_TIME': 'Load Time', 'ROW_PARSED': 'No. of Rows Loaded'},
+             title = 'Successful Load History')
+
+fig_load_hist_success.update_layout(
+    xaxis_title = '',
+    yaxis_title = 'No. of Rows Loaded',
+    xaxis_tickformat = '%Y-%m-%d',
+    bargap = 0.2,
+    width = 800,
+    height = 400,
+    title = {
+        'text': 'Successful Load History',
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    }
+)
+
+# Display the Plotly chart in Streamlit
+st.plotly_chart(fig_load_hist_success, use_container_width = True)
+
+# All load history query
+if selected_db and selected_schema and selected_tb:
+    load_hist_all_query ='''
+        SELECT 
+            CATALOG_ID,
+            CATALOG_NAME,
+            SCHEMA_ID,
+            SCHEMA_NAME,
+            TABLE_ID,
+            TABLE_NAME,
+            FILE_NAME,
+            LAST_LOAD_TIME,
+            STATUS,
+            ROW_COUNT,
+            ROW_PARSED
+        FROM SNOWFLAKE.ACCOUNT_USAGE.LOAD_HISTORY
+        WHERE CATALOG_NAME = '{}' 
+        AND SCHEMA_NAME = '{}'
+        AND TABLE_NAME = '{}'
+        ORDER BY LAST_LOAD_TIME DESC
+    '''.format(
+        selected_db,
+        selected_schema,
+        selected_tb
+    )
+else:
+    load_hist_all_query ='''
+        SELECT 
+            NULL AS CATALOG_ID,
+            NULL AS CATALOG_NAME,
+            NULL AS SCHEMA_ID,
+            NULL AS SCHEMA_NAME,
+            NULL AS TABLE_ID,
+            NULL AS TABLE_NAME,
+            NULL AS FILE_NAME,
+            NULL AS LAST_LOAD_TIME,
+            NULL AS STATUS,
+            0 AS ROW_COUNT,
+            0 AS ROW_PARSED
+    '''
+
+# Convert to DataFrame
+df_load_hist_all = session.sql(load_hist_all_query).to_pandas()
+
+# Display all load history DataFrame
+st.dataframe(df_load_hist_all, use_container_width = True)
